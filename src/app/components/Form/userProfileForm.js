1aab6e2ba4c7a89d2916fa29ff0b45f27caf7c47
Aleeza Jogiyat <aleeza.jogiyat@savyour.com>
2023-12-06 22:55:30 +0500

    minor change

--- src/app/components/Form/userProfileForm.js
+++ src/app/components/Form/userProfileForm.js
@@ -4,46 +4,57 @@ import { useNavigate } from 'react-router';
 import { useMediaQuery } from 'react-responsive';
 import { RiMailFill, RiUser2Line, RiDragMove2Fill } from 'react-icons/ri';
 import { Col } from 'antd';
+import MobileHeader from '../Header/MobileHeader';
+import { routePaths } from '../../routes/config';
 
 const Profile = ({ title, showDrawer, data }) => {
     const navigate = useNavigate()
     const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
 
     return (
-        <>
-            <div class="container">
-                <div className='profileHeader'>
-                    <h1>Profile</h1>
-                </div>
-                <div  class="profile">
-                        <header class="profile-header">
-                            <div class="details">
-                                <img src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=b38c22a46932485790a3f52c61fcbe5a" alt="John Doe" class="profile-pic"/>
-                                <h1 class="heading">{data.userName}</h1>
-                                <div class="location">       
-                                    <p>{data.userId}</p>
-                                </div>
-                                <div class="stats">
-                                    <Col
-                                        // span={10}
-                                        offset={isMobile ? 0 : 4}
-                                        md={10} sm={16}>
-                                        <div class="col-4">
-                                            <h4><RiUser2Line/></h4>
-                                            <p>{data.role}</p>
+        <div>
+            {
+                isMobile ? (<MobileHeader
+                route={routePaths.Admin.login}
+                showDrawer={showDrawer}
+                />
+                ) : (
+                    <div class="container">
+                        <div className='profileHeader'>
+                            <h1>Profile</h1>
+                        </div>
+                        <div  class="profile">
+                                <header class="profile-head">
+                                    <div class="details">
+                                        <img src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=b38c22a46932485790a3f52c61fcbe5a" alt="John Doe" class="profile-pic"/>
+                                        <h1 class="heading">{data.userName}</h1>
+                                        <div class="location">       
+                                            <p>{data.userId}</p>
                                         </div>
+                                        <div class="stats">
+                                            <Col
+                                                // span={10}
+                                                offset={isMobile ? 0 : 4}
+                                                md={10} sm={16}>
+                                                <div class="col-4">
+                                                    <h4><RiUser2Line/></h4>
+                                                    <p>{data.role}</p>
+                                                </div>
 
-                                        <div class="col-4">
-                                            <h4><RiMailFill /></h4>
-                                            <p>{data.email}</p>
+                                                <div class="col-4">
+                                                    <h4><RiMailFill /></h4>
+                                                    <p>{data.email}</p>
+                                                </div>
+                                                </Col>
                                         </div>
-                                        </Col>
-                                </div>
-                            </div>
-                        </header>
-                </div>
-            </div>
-        </>
+                                    </div>
+                                </header>
+                        </div>
+                    </div>
+                )
+            }
+            
+        </div>
     )
 }
 
