169f0ae7b48e68acfb3e4178bf19dc6969c826fb
“ALEEZAJOGIYAT” <alizajogyat09@gmail.com>
2023-12-11 00:24:26 +0500

    fixed UI issues of profile form

--- src/app/components/Form/userProfileForm.js
+++ src/app/components/Form/userProfileForm.js
@@ -2,58 +2,69 @@ import React, { useState } from 'react'
 import './profile.css';
 import { useNavigate } from 'react-router';
 import { useMediaQuery } from 'react-responsive';
-import { RiMailFill, RiUser2Line, RiDragMove2Fill } from 'react-icons/ri';
+import { RiMailFill, RiUser2Line, RiDragMove2Fill, RiPhoneFill } from 'react-icons/ri';
 import { Col } from 'antd';
 import MobileHeader from '../Header/MobileHeader';
 import { routePaths } from '../../routes/config';
+import { Header } from '../Header';
+import { Button } from 'antd'
+import { ExportOutlined } from '@ant-design/icons';
+import avatar from '../../../assets/images/avatar.jpg'
+
 
 const Profile = ({ title, showDrawer, data }) => {
     const navigate = useNavigate()
     const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
+    const handleLogout = () => {
+        navigate(routePaths.Admin.login);
+    }
 
     return (
         <div>
-            {
-                isMobile ? (<MobileHeader
-                route={routePaths.Admin.login}
-                showDrawer={showDrawer}
-                />
-                ) : (
-                    <div class="container">
-                        <div className='profileHeader'>
-                            <h1>Profile</h1>
-                        </div>
-                        <div  class="profile">
-                                <header class="profile-head">
-                                    <div class="details">
-                                        <img src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=b38c22a46932485790a3f52c61fcbe5a" alt="John Doe" class="profile-pic"/>
-                                        <h1 class="heading">{data.userName}</h1>
-                                        <div class="location">       
-                                            <p>{data.userId}</p>
-                                        </div>
-                                        <div class="stats">
-                                            <Col
-                                                // span={10}
-                                                offset={isMobile ? 0 : 4}
-                                                md={10} sm={16}>
-                                                <div class="col-4">
-                                                    <h4><RiUser2Line/></h4>
-                                                    <p>{data.role}</p>
-                                                </div>
-
-                                                <div class="col-4">
-                                                    <h4><RiMailFill /></h4>
-                                                    <p>{data.email}</p>
-                                                </div>
-                                                </Col>
-                                        </div>
+            {isMobile ? <MobileHeader route={routePaths.Admin.login} showDrawer={showDrawer} /> :
+            <div className='profileLogoutBtn'>
+                <Button type="text" className='button'
+                    onClick={handleLogout}
+                >
+                    Logout
+                </Button>
+            </div>
+        }
+            <div class="container">
+                <div className='profileHeader'>
+                    <h2>Profile</h2>
+                </div>
+                <div  class="profile">
+                    <header class="profile-head">
+                        <div class="details">
+                            <img src={avatar} alt="John Doe" class="profile-pic"/>
+                            <h1 class="heading">{data.userName}</h1>
+                            <div class="location">       
+                                <p>{data.userId}</p>
+                            </div>
+                            <div class="stats">
+                                <Col
+                                    // span={10}
+                                    offset={isMobile ? 0 : 4}
+                                    md={10} sm={16}>
+                                    <div class="col-4" >
+                                        <h4><RiUser2Line/></h4>
+                                        <p>{data.role}</p>
                                     </div>
-                                </header>
+                                    <div class="col-4" >
+                                        <h4><RiMailFill /></h4>
+                                        <p>{data.email}</p>
+                                    </div>
+                                    <div class="col-4" >
+                                        <h4><RiPhoneFill/></h4>
+                                        <p>{data.contact}</p>
+                                    </div>
+                                    </Col>
+                            </div>
                         </div>
-                    </div>
-                )
-            }
-            
+                    </header>
+                </div>
+            </div>            
         </div>
     )
 }
--- src/app/roles/User/UserProfile.js
+++ src/app/roles/User/UserProfile.js
@@ -5,9 +5,34 @@ import Profile from "../../components/Form/userProfileForm";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { CustomAlert } from "../../components/Alert";
+import { useMediaQuery } from "react-responsive";
+import { getItem } from '../../utils/functions';
+import { FaThList, FaWarehouse, FaBuilding } from 'react-icons/fa';
+import { HiUserAdd } from 'react-icons/hi';
+import { BiMessageError } from 'react-icons/bi';
+import { MdOutlineDomainDisabled } from 'react-icons/md';
+import { routePaths } from '../../routes/config';
+import MobileHeader from '../../components/Header/MobileHeader';
+import SideBar from "../../components/Layouts/SideBar";
+
 
 const UserProfile = () => {
   const [tenantData, setTenantData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+      setOpen(true);
+  };
+
+
+  const items = [
+      getItem('Tenant', '1', <FaWarehouse />,
+          [getItem('Add Complaint', 'addcomplaint', <HiUserAdd />),
+          getItem('List Complaint', 'complaintlist', <FaThList />)]),
+      getItem('Receipts', '2', <MdOutlineDomainDisabled />,
+      [getItem('List Receipts', 'receiptList', <BiMessageError />)]
+      ),
+  ];
+  const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
 
   const tenantId = localStorage.getItem("tenantId");
 
@@ -26,7 +51,8 @@ const UserProfile = () => {
 
   return (
     <div>
-      <Dashboard data={<Profile data={tenantData} />} tenantData={tenantData} />
+      <SideBar children={<Profile data={tenantData} showDrawer={showDrawer}/>} items={items} showDrawer={showDrawer} open={open} setOpen={setOpen}/>
+      {/* <Dashboard data={<Profile data={tenantData} />} tenantData={tenantData} /> */}
       <CustomAlert />
     </div>
   );
